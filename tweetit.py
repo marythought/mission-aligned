7a7bdf4c510a96cb0297536696e2925a7c211b2d
Mary Dickson <mdickson@gmail.com>
2015-04-18 01:18:29 -0700

    notes for next steps

--- tweetit.py
+++ tweetit.py
@@ -3,8 +3,13 @@
 with open('workfile.txt', "r") as f:
     lines = f.readlines()
 
+# Test to see how many lines are too long to tweet
+
 for x in range(101):
     if len(lines[x]) > 140:
         print "too long"
     else:
         print "all good"
+
+# Import code from previous python bot that uses tweepy to
+# read and tweet random line. Add a "try" line to test for tweet-length?
